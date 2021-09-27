46e5f40241d33fb0ca03b94472ba7f1a7750989f
vsz <>
2021-09-27 10:14:06 -0700

    demo

--- app/leeroy-web/web.go
+++ app/leeroy-web/web.go
@@ -19,7 +19,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	log.Print("Demo - leeroy web server is ready!")
+	log.Print("Demo - leeroy web server ready!")
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
